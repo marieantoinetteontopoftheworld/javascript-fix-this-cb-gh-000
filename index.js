49a8ccbda3d29dc93fbb89244dabce23553436d8
Jack Heseltine <jack.heseltine@gmail.com>
2017-11-03 17:55:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ function cool(updateFunction) {
 function makeDessert() {
   //add code here to decide which make... function to call
   //based on which link was clicked
+  alert(this.parentNode.id);
   if (this.parentNode.id === 'cake') {
     makeCake(this.parentNode);
   } else {
